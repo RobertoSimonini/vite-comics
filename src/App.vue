81412aa4861e3e2a82693d7cf1fb62a05dea492d
RobertoSimonini <robertosimonini1@gmail.com>
2023-01-20 16:40:23 +0100

    added logo and links

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  name: 'comics', 
+  components: {AppHeader}
+}
+</script>
+
+
+<template>
+<app-header></app-header>
+</template>
+
+<style>
+  /* Generics  */
+
+  ul {
+    list-style-type: none;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,91 @@
+<script>
+export default {
+    data () {
+        return {
+          links :  [
+            {
+                text: 'Characters',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Comics',
+                url: '#',
+                current: true,
+            },
+            {
+                text: 'Movies',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'TV',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Games',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Collectibles',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Videos',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Fans',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'News',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Shop',
+                url: '#',
+                current: false,
+            },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+        <nav>
+            <ul>
+                <li v-for="link in links">
+                     <a href="#"> {{ link.text }} </a> 
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+    header {
+        height: 150px;
+        display: flex;
+        justify-content: space-between;
+
+        ul {
+            display: flex;
+            li {
+                margin: 0 1rem;
+            }
+        }
+    }
+</style>
