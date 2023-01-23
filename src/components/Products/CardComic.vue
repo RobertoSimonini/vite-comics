4304590232f4c1ee5bd205a07e33b123ca030f05
RobertoSimonini <robertosimonini1@gmail.com>
2023-01-23 17:43:11 +0100

    Added hover effect to the cards

--- src/components/Products/CardComic.vue
+++ src/components/Products/CardComic.vue
@@ -16,18 +16,26 @@ export default {
 
 
 <style lang="scss" scoped>
-    .comic-card {
-        margin: 1.5rem 0;
+.comic-card {
+    margin: 1.5rem 0;
+    transition: transform 0.25s;
+
+    &:hover {
+        transform: scale(1.05);
+    }
 
         img {
             display: block;
             height: 200px;
             width: 200px;
+            cursor: pointer;         
         }
+
 
         h4 {
             text-transform: uppercase;
             font-size: 15px;
             }
-    }
+}
+
 </style>
