5c196799b50241f04cc6888ff88f723dca0b4a6e
RobertoSimonini <robertosimonini1@gmail.com>
2023-01-24 09:00:53 +0100

    Added new Series button

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,21 +1,33 @@
 <script>
+import NewSeries from './buttons/NewSeries.vue';
 export default {
-    name: 'Jumbotron'
+    name: 'Jumbotron',
+    components: {
+        NewSeries
+    }
 }
 </script>
 
 <template>
     <section>
-        
+        <new-series></new-series>
     </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
     section {
         height: 400px;
         background-image: url(../assets/img/jumbotron.jpg);
         background-size: cover;
         background-repeat: no-repeat;
+
+        position: relative;
+        
+        button {
+            position: absolute;
+            bottom: -5%;
+            left: 10%;
+        }
     }
 
 </style>
---
+++ src/components/buttons/NewSeries.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+}
+</script>
+
+
+
+<template>
+    <button class="new-series">
+        New Series
+    </button>
+</template>
+
+<style scoped>
+    .new-series {
+            background-color: #0282F9;
+            padding: 10px 30px;
+            text-transform: uppercase;
+            color: #fff;
+            font-size: 15px;
+            text-align: center;
+            cursor: pointer;
+        }
+
+</style>
